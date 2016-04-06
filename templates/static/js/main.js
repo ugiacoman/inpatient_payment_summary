9ea49bf1b395399e1e8139857b0f90d63fbf2010
Ulises Giacoman <ugiacoman@email.wm.edu>
2016-04-05 20:08:15 -0400

    restructered

---
+++ templates/static/js/main.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidWdpYWNvbWFuIiwiYSI6ImNpbW8xdTUwejAwYXF1N2x5dmRydnpuZTIifQ.YF2LyOa-5Ookoi4_kpmCEg';
+var map = new mapboxgl.Map({
+      container: 'map',
+          style: 'mapbox://styles/mapbox/dark-v8',
+              zoom: 4,
+                  center: [-98.873,39.213]
+});
