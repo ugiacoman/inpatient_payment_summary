a9190e0c70bda7b04e1ea3bd2ccd0cbd4ca1b63a
Ulises Giacoman <ugiacoman@email.wm.edu>
2016-04-08 21:28:19 -0400

    added framework for db

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # inpatient_payment_summary
 Inpatient Prospective Payment System Provider Summary for Top 100 Diagnoses FY2011
+
+
+# How to Setup DB
+
+	1. Run python interpretor `$ python` and enter following commands
+		```python 
+		>>> from app import db
+		```
+		```python 
+		>>> db.create_all()
+		```
+	2. This will setup blank tables. Note: You'll have to change line 6 
+	`'postgresql://<user>:<password>@localhost/<dbname>'`
--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='templates/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:st@localhost/test'
+db = SQLAlchemy(app)
+
+class Incident(db.Model):
+	incident_id = db.Column(db.Integer, primary_key=True)
+	type = db.Column(db.String(100))
+
+	def __init__(self, incident_id, type):
+		self.incident_id = incident_id
+		self.type = type
 
 @app.route("/")
 def index():
