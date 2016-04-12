f8ba59dc2175458135e9b530a7d7ce32d41bf31f
Ulises Giacoman <ugiacoman@email.wm.edu>
2016-04-11 21:49:05 -0400

    updated models

--- models.py
+++ models.py
@@ -1,12 +1,51 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
+
+
 class Diagnosis(db.Model):
 	procedure = db.Column(db.Integer, primary_key=True)
-	avg_total_payments = db.Column(db.Money)
-	avg_total_payments = db.Column(db.Money) 
-	provider_id = db.Column(db.Integer)
+ 	provider_id = db.Column(db.Integer)
 
-	def __init__(self, incident_id, type):
-		self.incident_id = incident_id
-		self.type = type
+	avg_total_payments = db.Column(db.Numeric(precision=2, asdecimal=True))
+	avg_medicare_payments = db.Column(db.Numeric(precision=2, asdecimal=True))
+	avg_covered_charges = db.Column(db.Numeric(precision=2, asdecimal=True))
+	total_discharge = db.Column(db.Numeric(precision=2, asdecimal=True))
+
+	def __init__(self, procedure, provider_id, avg_medicare_payments, avg_total_payments, 
+		avg_covered_charges, total_discharge):
+		self.procedure = procedure
+		self.provider_id = provider_id
+		self.avg_total_payments = avg_total_payments
+		self.avg_medicare_payments = avg_medicare_payments
+		self.avg_covered_charges = avg_covered_charges
+		self.total_discharge = total_discharge
+
+'''
+Diagnosis Table:
+
+	procedure: 
+	provider_id: (Foreign key references id in Provider Table)
+	avg_total_payments:
+	avg_medicare_payments:
+	avg_covered_charges:
+	total_discharge: 
+
+'''
+class Diagnosis(db.Model):
+	procedure = db.Column(db.Integer, primary_key=True)
+ 	provider_id = db.Column(db.Integer)
+
+	avg_total_payments = db.Column(db.Numeric(precision=2, asdecimal=True))
+	avg_medicare_payments = db.Column(db.Numeric(precision=2, asdecimal=True))
+	avg_covered_charges = db.Column(db.Numeric(precision=2, asdecimal=True))
+	total_discharge = db.Column(db.Numeric(precision=2, asdecimal=True))
+
+	def __init__(self, procedure, provider_id, avg_medicare_payments, avg_total_payments, 
+		avg_covered_charges, total_discharge):
+		self.procedure = procedure
+		self.provider_id = provider_id
+		self.avg_total_payments = avg_total_payments
+		self.avg_medicare_payments = avg_medicare_payments
+		self.avg_covered_charges = avg_covered_charges
+		self.total_discharge = total_discharge		
