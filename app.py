d184d22afd5322cf029622bd1fe1915aea7c2256
Ulises Giacoman <ugiacoman@email.wm.edu>
2016-04-27 01:13:32 -0400

    added min cost

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 
 from models import *
 
+
 def sql_count(query):
 	sql = text(query)
 	result = db.engine.execute(sql)
@@ -20,7 +21,7 @@ def sql_count(query):
 		names.append(row[0])
 	return jsonify(count=str(names[0]))
 
-def sql_select(query):
+def sql_geojson(query):
 	sql = text(query)
 	result = db.engine.execute(sql)
 	geojson_dict = {}
@@ -28,18 +29,30 @@ def sql_select(query):
 	years = []
 	dict_result = {}
 	for row in result:
-		primary_id = row['provider_id']
+		primary_id = float(row['avg_total_payments'])
 		secondary_id = row['name'].encode("utf-8")
 		lat = float(row['longitude'])
 		lng = float(row['latitude'])
 		feature = { "type": "Feature", "properties": { "Primary ID": secondary_id, "Secondary ID": primary_id }, "geometry": { "type": "Point", "coordinates": [ lng, lat ] } }
-		features.append(feature)
-	print(features)		
+		features.append(feature)	
 	result.close()
 	geojson_dict["type"] = "FeatureCollection"
 	geojson_dict["crs"] = { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } }
 	geojson_dict["features"] = features
 	return str(geojson_dict).replace("'", '"')
+
+def sql_json(query):
+	sql = text(query)
+	result = db.engine.execute(sql)
+	procedures = {}
+	count = 0
+	for row in result:
+		procedure = str(row).strip("(u'");
+		procedure = procedure.strip("',)");
+		procedures[count] = procedure
+		count += 1
+	result.close()
+	return jsonify(procedures)
 
 @app.route("/test-count")
 def test_count():
@@ -49,14 +62,26 @@ def test_count():
 @app.route("/all-locations")
 def test_select():
 	query = sql_select("select location.provider_id,name,latitude,longitude from location,provider where location.provider_id = provider.provider_id;")
-	# f = open(app.static_folder + "/data/test.geojson", 'w')
-	# f.write(query)
-	# f.close()	
 	return query
 
 @app.route("/filter")
 def filter():
     return render_template('filter.html')
+
+@app.route('/mincost/<drc>')
+def min_cost(drc):
+	drc = "039 - EXTRACRANIAL PROCEDURES W/O CC/MCC"
+	query = sql_geojson("select c.name, b.longitude, b.latitude, a.avg_total_payments from diagnosis a, location b, provider c where a.procedure = '039 - EXTRACRANIAL PROCEDURES W/O CC/MCC' and avg_total_payments = (select min(avg_total_payments) from diagnosis where diagnosis.procedure = '{}') and b.provider_id = a.provider_id and c.provider_id = a.provider_id;".format(drc))
+	return query
+
+@app.route('/procedures')
+def procedures():
+	query = sql_json("select DISTINCT procedure from diagnosis")
+	return query	
+
+@app.route('/min')
+def min():
+	return render_template('min_cost.html')	
 
 @app.route("/")
 def index():
